6d095a4df68946e9282fef34373dc840107ad2ec
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-10 17:22:51 +0300

    Update modal.js

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,3 +1,5 @@
+// import fetchMovie from './MovieApi';
+
 const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   backdrop: document.querySelector('[data-modal]'),
@@ -5,26 +7,37 @@ const refs = {
 };
 
 const toggleActiveBtn = () => refs.openModalBtn.classList.toggle('btn-is-active');
+const toggleBackdrop = () => refs.backdrop.classList.toggle('is-hidden');
 
 const onEscClick = e => {
   if (e.code === 'Escape') {
-    refs.backdrop.classList.add('is-hidden');
-    document.removeEventListener('keydown', onEscClick);
     toggleActiveBtn();
+    toggleBackdrop();
+    document.removeEventListener('keydown', onEscClick);
+    console.log(1);
   }
 };
 
-const toggleModal = () => {
-  refs.backdrop.classList.toggle('is-hidden');
-  document.addEventListener('keydown', onEscClick);
+const openModal = () => {
   toggleActiveBtn();
+  toggleBackdrop();
+  document.addEventListener('keydown', onEscClick);
 };
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeBtn.addEventListener('click', toggleModal);
+
+refs.openModalBtn.addEventListener('click', openModal);
+
+const closeModal = () => {
+  toggleActiveBtn();
+  toggleBackdrop();
+  document.removeEventListener('keydown', onEscClick);
+};
+
+refs.closeBtn.addEventListener('click', closeModal);
 
 const onBackdropClick = e => {
   if (e.currentTarget === e.target) {
-    toggleModal();
+    closeModal();
   }
 };
+
 refs.backdrop.addEventListener('click', onBackdropClick);
